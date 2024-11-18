eb9f2bfe6669a62b65d13916306a20cde1973983
vitor <wytorh@gmail.com>
2024-11-18 14:29:37 -0300

    segunda-feira v13

--- SemChrome.py
+++ SemChrome.py
@@ -2,23 +2,20 @@ import os
 import serial
 import datetime
 from flask import Flask, render_template, jsonify
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 # Função para listar os dispositivos seriais disponíveis
 def listar_dispositivos_seriais():
     serial_dir = "/dev/serial/by-id/"
-    dispositivos = []
-
-    if os.path.exists(serial_dir):
-        for device_path in os.listdir(serial_dir):
-            dispositivos.append(os.path.join(serial_dir, device_path))
-    return dispositivos
+    return [os.path.join(serial_dir, device_path) for device_path in os.listdir(serial_dir)] if os.path.exists(serial_dir) else []
 
 # Função para interpretar a string de dados do rastreamento
 def interpretar_string(dados):
     partes = dados.split(';')
-    rastreamento = {
+    return {
         "SerialRastreador": partes[0].split('#')[1],
         "DataHoraRastreador": datetime.datetime.strptime(partes[1], "%d-%m-%Y %H:%M:%S"),
         "Latitude": float(partes[3]),
@@ -49,35 +46,32 @@ def interpretar_string(dados):
         "VelocidadeMaxMicroCerca": int(partes[51]),
         "dadosBrutos": dados
     }
-    return rastreamento
 
 # Função para obter os dados do rastreador
 def obter_dados_rastreamento():
     dispositivos = listar_dispositivos_seriais()
-    dados_rastreamento = None
+    if not dispositivos:
+        return "Nenhum dispositivo serial encontrado."
 
-    if dispositivos:
+    try:
         port_path = dispositivos[0]
-        try:
-            # Conectar ao dispositivo serial
-            ser = serial.Serial(port_path, 19290, timeout=1)
-            comando = "#STATUS\n"
-            ser.write(comando.encode())
-
-            # Ler a resposta
+        with serial.Serial(port_path, 19290, timeout=1) as ser:
+            ser.write("#STATUS\n".encode())
             response = ser.readline().decode('utf-8', errors='ignore').strip()
-            if response:
-                try:
-                    dados_rastreamento = interpretar_string(response)
-                except Exception as e:
-                    dados_rastreamento = f"Erro ao interpretar dados: {e}"
+            return interpretar_string(response) if response else "Sem dados recebidos."
+    except Exception as e:
+        return f"Erro ao acessar o dispositivo serial: {e}"
 
-            # Fechar a porta serial
-            ser.close()
-        except Exception as e:
-            dados_rastreamento = f"Erro ao abrir a porta serial: {e}"
-
-    return dados_rastreamento
+# Configura o serviço do ChromeDriver
+chromedriver_path = '/usr/bin/chromedriver'
+service = Service(chromedriver_path)
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--disable-software-rasterizer")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(service=service, options=options)
 
 # Rota principal para exibir a página index
 @app.route('/')
@@ -87,8 +81,7 @@ def index():
 # Rota para retornar os dados do rastreador como JSON
 @app.route('/dados_rastreamento')
 def dados_rastreamento():
-    dados = obter_dados_rastreamento()
-    return jsonify(dados)
+    return jsonify(obter_dados_rastreamento())
 
 # Iniciar a aplicação Flask
 if __name__ == '__main__':
